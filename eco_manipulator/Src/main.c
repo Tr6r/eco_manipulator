ebcd1a63a6cf550fb8caac80bb06fd4343d6956c
Đinh Quốc Cường <tr6r20@gmail.com>
2025-03-03 11:17:50 +0700

    create header file stm32f411ce.h

--- eco_manipulator/Src/main.c
+++ eco_manipulator/Src/main.c
@@ -17,10 +17,7 @@
  */
 
 #include <stdint.h>
-
-#if !defined(__SOFT_FP__) && defined(__ARM_FP)
-  #warning "FPU is not initialized, but the project is compiling for an FPU. Please initialize the FPU before use."
-#endif
+#include <../drivers/Inc/stm32f411ce.h>
 
 int main(void)
 {
---
+++ eco_manipulator/drivers/Inc/stm32f411ce.h
@@ -0,0 +1,13 @@
+/*
+ * stm32f411ce.h
+ *
+ *  Created on: Mar 3, 2025
+ *      Author: tr6r2
+ */
+
+#ifndef INC_STM32F411CE_H_
+#define INC_STM32F411CE_H_
+
+
+
+#endif /* INC_STM32F411CE_H_ */
